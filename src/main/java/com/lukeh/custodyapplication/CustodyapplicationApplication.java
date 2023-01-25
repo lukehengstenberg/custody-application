984d4b6889198f0f6b51beb51bc806c0a2371ee6
Luke Hengstenberg <lukehengstenberg@gmail.com>
2023-01-25 11:44:11 +0000

    Initialise Custody Application #1

---
+++ src/main/java/com/lukeh/custodyapplication/CustodyapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.lukeh.custodyapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustodyapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustodyapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lukeh/custodyapplication/CustodyapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukeh.custodyapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustodyapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
